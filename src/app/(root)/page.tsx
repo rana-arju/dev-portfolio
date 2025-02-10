0e00f551af1d475b202e5825ee679762dfc89918
rana-arju <arjurana20@gmail.com>
2025-02-10 19:51:01 +0600

    error fix

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -5,23 +5,43 @@ import Skills from "@/components/Skills";
 import Blogs from "@/components/Blogs";
 
 export default async function Home() {
-  const response = await fetch("http://localhost:5000/api/v1/project", {
-    method: "GET",
-    headers: { "Content-Type": "application/json" },
-  });
-  const projects = await response.json();
-   const res = await fetch("http://localhost:5000/api/v1/blog", {
-     method: "GET",
-     headers: { "Content-Type": "application/json" },
-   });
-   const blogPosts = await res.json();
+  let projects =[];
+  let blogPosts = [];
+
+  try {
+    const response = await fetch("http://localhost:5000/api/v1/project", {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const result = await response.json();
+    projects = result?.data || [];
+
+    const res = await fetch("http://localhost:5000/api/v1/blog", {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    });
+    if (!res.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const result2 = await res.json();
+    blogPosts = result2?.data;
+
+    console.log("Projects fetched successfully:", projects);
+  } catch (error) {
+    console.error("Error fetching projects:", error);
+  }
+
   return (
     <div>
       <Hero />
       <AboutSection page="" />
       <Skills />
-      <Projects projects={projects?.data?.slice(0, 3)} />
-      <Blogs blogPosts={blogPosts?.data?.slice(0, 3)} />
+      <Projects projects={projects?.slice(0, 3)} />
+      <Blogs blogPosts={blogPosts?.slice(0, 3)} />
     </div>
   );
 }
--- src/app/(root)/projects/page.tsx
+++ src/app/(root)/projects/page.tsx
@@ -2,11 +2,25 @@ import { Projects } from "@/components/Projects";
 import React from "react";
 
 const ProjectPage = async () => {
-  const response = await fetch(`${process.env.BACKEND_URL}/project`, {
-    method: "GET",
-    headers: { "Content-Type": "application/json" },
-  });
-  const projects = await response.json();
+  let projects;
+  try {
+    const response = await fetch(`${process.env.BACKEND_URL}/project`, {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    projects = result?.data || [];
+
+    console.log("Projects fetched successfully:", projects);
+  } catch (error) {
+    console.error("Error fetching projects:", error);
+    projects = []; // Provide a fallback value
+  }
 
   return (
     <main className="pt-10">
